f3f742be2b5867829983e9bf7df55969a5d52cd4
Louis <Louis.Medina@flatironschool.com>
2022-08-08 16:36:13 -0400

    add notes for 01 lecture

--- 01_Functions_&_Scope/src/index.js
+++ 01_Functions_&_Scope/src/index.js
@@ -1,8 +1,51 @@
+// Topics
+
+// Declaring vs. Invoking Functions
+
+    // var myVariable = "Test";
+
+    // console.log(myVariable);
+
+    
+    // function myFunction()
+    
+    // Function Declaration
+    // Defining a Spell
+    // function sayHello() {
+    //     console.log("Hello!");
+    // }
+
+    // sayHello();
+
+    // function sayHello() {
+    //     console.log("Hello!");
+    // }
+
+    // Cannot Be Redeclared or Reassigned
+    // const sayHello = () => console.log("Hello!");
+
+    // Cannot Reassign Value for sayHello const
+    // sayHello = "Something New";
+
+    // Cannot Redeclare sayHello const
+    // const sayHello = "Something New";
+
+    // Function Invocation
+    // Casting a Spell
+    // sayHello();
+    
+
+// Scope
+    // Global
+    // Function
+    // Block
+
 //Data 
+
 const inventory = [
         {
             id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+            title: 'NEW TITLE!',
             author: 'Marjin Haverbeke',
             price: 10.00,
             reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
@@ -65,6 +108,156 @@ const inventory = [
             reviews: [{userID: 99, content:'One of the most helpful books for taking on the tech interview'}, {userID:20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
             inventory: 20,
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-
         }
     ]
+
+    // Bracket Notation
+    // console.log(inventory[0]["title"]);
+
+    // Dot Notation
+    // Readability => More Legible
+    // Writability => Easier / Less Error Prone
+    // inventory[0].title = "My Newest Title"
+    // inventory[0]["title"] = "My Newest Title"
+
+    // console.log(inventory[0].title)
+
+    // function doSomething() {
+    //     console.log("Something");
+    // }
+
+    // doSomething();
+
+    // Create a function that's going to console.log / format the price of any given book
+    // Example => For first book, "$10.00"
+
+    // book => Parameter
+    // Declarations Go With Parameters
+    // function priceFormatter(myBook, second="Hello", third="World") {
+    
+    // MDN + toFixed => Additional Documentation
+    function priceFormatter(myBook) {
+        // var price = myBook.price;
+
+        // toString();
+        // console.log(price.toString());
+        console.log(`$${myBook.price.toFixed(2)}`);
+    }
+
+    // inventory[0] => Argument
+    // Invocations Go With Arguments
+    // priceFormatter(inventory[0]);
+
+    // Create a function that pulls Book Author / Title to create a full string
+    // Example => "JavaScript & JQuery: Interactive Front-End Web Development by Jon Duckett is on sale!"
+    // function outputTitleAndAuthor(myBook) {
+    //     // console.log(`${myBook.title} by ${myBook.author} is on sale!`);
+    //     console.log(`${myBook.title} by ${myBook.author} is on sale!`);
+    // }
+
+    // Implicit Return
+    // const outputTitleAndAuthor = myBook => `${myBook.title} by ${myBook.author} is on sale!`;
+    
+    // Explicit Return
+    
+    // Global Scope
+    // const myString = "Hello!";
+
+    // const outputTitleAndAuthor = myBook => { 
+    //     var fullString = `${myBook.title} by ${myBook.author} is on sale!`;
+
+    //     console.log(myString);
+
+    //     return fullString;
+    // }
+    
+    // // console.log(fullString);
+
+    // let returnValue = outputTitleAndAuthor(inventory[1]);
+
+    // // ...
+    // // ...
+    // // ...
+
+    // returnValue = "New Value";
+
+    // console.log(returnValue);
+
+    // Create a function to build a new Book object with specific attributes
+    // Attributes:
+        // Title
+        // Price
+        // Author
+        // ImageUrl
+
+    // {
+    //     id:7,
+    //     title: 'Cracking the Coding Interview',
+    //     author: 'Gayle Laakmann McDowell',
+    //     price: 49.95,
+    //     reviews: [{userID: 99, content:'One of the most helpful books for taking on the tech interview'}, {userID:20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
+    //     inventory: 20,
+    //     imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
+    // }
+
+    const newTitle = "New Book Title";
+
+    function buildBook(title, price, author, imageUrl, reviews=[], stock=0) {
+        const newBook = {};
+
+        // id
+        newBook.id = inventory.length + 1;
+        
+        // inventory.length = inventory.length + 1;
+        // newBook.id = inventory.length ++;
+        
+        // title
+        newBook.title = title;
+
+        // price
+        newBook.price = price;
+        
+        // author
+        newBook.author = author;
+
+        // reviews
+        newBook.reviews = reviews;
+        
+        // inventory
+        newBook.inventory = stock;
+        
+        // imageUrl
+        // If an imageUrl is passed, then we want to set that as the Book's imageUrl
+        if(imageUrl) {
+            newBook.imageUrl = imageUrl;
+        } else {
+            // If not, we want to set a default imageUrl
+            newBook.imageUrl = "Default URL";
+        }
+        
+        inventory.push(newBook);
+
+        return inventory;
+    }
+
+    // inventory.length = 7
+    // buildBook(newTitle, 15, "First Author", 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg');
+    // // inventory.length = 8
+    
+    // // inventory.length = 8
+    // buildBook(newTitle, 20, "Second Author", 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg', [{title: "First Review"}], 5);
+    // // inventory.length = 9
+
+    // buildBook(newTitle, 10, "Third Author", "test", "secondTest");
+
+    // console.log(inventory);
+
+    function callbackFunction() {
+        console.log("Do Something!");
+    }
+
+    function higherOrderFunction(cb) {
+        cb();
+    }
+
+    higherOrderFunction(callbackFunction);
