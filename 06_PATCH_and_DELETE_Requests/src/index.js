7e22bc378a853522cd805bc4becaa22148007110
Louis <Louis.Medina@flatironschool.com>
2022-08-15 13:41:58 -0400

    add starter code for lecture 06

--- 06_PATCH_and_DELETE_Requests/src/index.js
+++ 06_PATCH_and_DELETE_Requests/src/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Function for making a GET request 
         function fetchResource(url){
             return fetch(url)
-            .then(res => res.json())
+            .then(res => res.json());
         }
 
         function createResources(url, body){
@@ -14,43 +14,46 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify(body),
             })
-            .then(res => res.json())
+            .then(res => res.json());
         }
 
     // Rendering functions
         // Renders Header
         function renderHeader(store){
-            document.querySelector('h1').textContent = store.name
+            document.querySelector('h1').textContent = store.name;
         }
         // Renders Footer
         function renderFooter(store){
-            const footerDivs = document.querySelectorAll('footer div')
-            footerDivs[0].textContent = store.name
-            footerDivs[1].textContent = store.address
-            footerDivs[2].textContent = store.hours
+            const footerDivs = document.querySelectorAll('footer div');
+            footerDivs[0].textContent = store.name;
+            footerDivs[1].textContent = store.address;
+            footerDivs[2].textContent = store.hours;
         }
     
         function renderBookCard(cardData) {
-            const li = document.createElement('li')
-            const h3 = document.createElement('h3')
-            const pAuthor = document.createElement('p')
-            const pPrice = document.createElement('p')
-            const img = document.createElement('img')
-            const btn = document.createElement('button')
+            const li = document.createElement('li');
+            const h3 = document.createElement('h3');
+            const pAuthor = document.createElement('p');
+            const pPrice = document.createElement('p');
+            const pInventory = document.createElement('input');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
     
-            h3.textContent = cardData.title
-            pAuthor.textContent = cardData.author
-            pPrice.textContent = `$${cardData.price}`
-            btn.textContent = 'Delete'
+            h3.textContent = cardData.title;
+            pAuthor.textContent = cardData.author;
+            pPrice.textContent = `$${cardData.price}`;
+            pInventory.type = 'number';
+            pInventory.value = cardData.inventory;
+            btn.textContent = 'Delete';
     
-            img.src = cardData.imageUrl
-            li.className = 'list-li'
+            img.src = cardData.imageUrl;
+            li.className = 'list-li';
     
             //Event Listeners 
-            btn.addEventListener('click',()=>li.remove())
+            btn.addEventListener('click',()=>li.remove());
         
-            li.append(h3,pAuthor,pPrice,img,btn)
-            document.querySelector('#book-list').append(li)
+            li.append(h3,pAuthor,pPrice,pInventory,img,btn);
+            document.querySelector('#book-list').append(li);
         }
     
     // Event Handlers
@@ -64,11 +67,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 imageUrl: e.target.imageUrl.value,
                 inventory:e.target.inventory.value,
                 reviews:[]
-            }
+            };
+
             createResources('http://localhost:3000/books', book)
             .then(renderBookCard)
-            .catch(e => console.error(e))
-
+            .catch(e => console.error(e));
         }
     
     
@@ -78,12 +81,11 @@ document.addEventListener('DOMContentLoaded', () => {
             renderHeader(store)
             renderFooter(store)
         })
-        .catch(e => console.error(e))
+        .catch(e => console.error(e));
     
         fetchResource('http://localhost:3000/books')
         .then(books => books.forEach(renderBookCard))
-        .catch(e => console.error(e))
+        .catch(e => console.error(e));
     
-        document.querySelector('#book-form').addEventListener('submit', handleForm)
-    
+        document.querySelector('#book-form').addEventListener('submit', handleForm);
 })
